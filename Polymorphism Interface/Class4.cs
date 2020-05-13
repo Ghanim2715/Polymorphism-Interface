99af58b2491eb4862ba20012cf8dc11ff600e6eb
Ghanim <muhammad.2715@students.amikom.ac.id>
2020-05-13 12:14:36 +0700

    Polymorphism Interface

---
+++ Polymorphism Interface/Class4.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class Canon : IPrinterWindows
+{
+    public override void show()
+    {
+        Console.WriteLine("Canon Display dimension : 9.5*12");
+    }
+
+    public override void print()
+    {
+        Console.WriteLine("Canon printer printing ...");
+    }
+}
